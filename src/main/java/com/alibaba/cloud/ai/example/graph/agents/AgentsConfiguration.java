a51bb85321f80b7b37228c4d532a083447a4baa9
zhouyou.ltx <zhouyou9505@gmail.com>
2025-05-19 19:32:52 +0800

    init coder node

--- src/main/java/com/alibaba/cloud/ai/example/graph/agents/AgentsConfiguration.java
+++ src/main/java/com/alibaba/cloud/ai/example/graph/agents/AgentsConfiguration.java
@@ -1,20 +1,20 @@
 package com.alibaba.cloud.ai.example.graph.agents;
 
 
-import io.modelcontextprotocol.client.McpAsyncClient;
+import com.alibaba.cloud.ai.example.graph.tool.PythonReplTool;
+import com.alibaba.cloud.ai.example.graph.tool.WebSearchTool;
 import io.modelcontextprotocol.client.McpSyncClient;
 import io.modelcontextprotocol.spec.McpSchema;
 import lombok.SneakyThrows;
+import org.springframework.ai.tool.method.MethodToolCallbackProvider;
 import org.springframework.core.io.Resource;
 import org.springframework.ai.chat.client.ChatClient;
-import org.springframework.ai.chat.model.ChatModel;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.util.FileCopyUtils;
+import org.springframework.util.CollectionUtils;
 
-import java.io.InputStreamReader;
 import java.nio.charset.Charset;
 import java.util.ArrayList;
 import java.util.List;
@@ -25,23 +25,62 @@ public class AgentsConfiguration {
 
     @Autowired
     private List<McpSyncClient> mcpSyncClients;  // For sync client
+    @Autowired
+    private PythonReplTool pythonReplTool;
+    @Autowired
+    private WebSearchTool webSearchTool;
 
     @Value("classpath:prompts/researcher.md")
     private Resource researcherPrompt;
+
+    @Value("classpath:prompts/coder.md")
+    private Resource coderPrompt;
+
 
     @SneakyThrows
     @Bean
     public ChatClient researchAgent(ChatClient.Builder chatClientBuilder) {
         List<McpSchema.Tool> mcpTools = new ArrayList<>();
 
-        for (McpSyncClient client : mcpSyncClients) {
-            McpSchema.ListToolsResult listToolsResult = client.listTools();
-            mcpTools.addAll(listToolsResult.tools());
+        if (CollectionUtils.isEmpty(mcpSyncClients)) {
+            for (McpSyncClient client : mcpSyncClients) {
+                McpSchema.ListToolsResult listToolsResult = client.listTools();
+                mcpTools.addAll(listToolsResult.tools());
+            }
+        }else {
+            MethodToolCallbackProvider build = MethodToolCallbackProvider.builder().toolObjects(webSearchTool).build();
+            chatClientBuilder.defaultToolCallbacks(
+                    build.getToolCallbacks()
+            ).build();
         }
 
         return chatClientBuilder
                 .defaultTools(mcpTools)
                 .defaultSystem(researcherPrompt.getContentAsString(Charset.defaultCharset()))
+                .build();
+    }
+
+
+    @SneakyThrows
+    @Bean
+    public ChatClient coderAgent(ChatClient.Builder chatClientBuilder) {
+        List<McpSchema.Tool> mcpTools = new ArrayList<>();
+
+        if (CollectionUtils.isEmpty(mcpSyncClients)) {
+            for (McpSyncClient client : mcpSyncClients) {
+                McpSchema.ListToolsResult listToolsResult = client.listTools();
+                mcpTools.addAll(listToolsResult.tools());
+            }
+            chatClientBuilder.defaultTools(mcpTools);
+        } else {
+            MethodToolCallbackProvider build = MethodToolCallbackProvider.builder().toolObjects(pythonReplTool).build();
+            chatClientBuilder.defaultToolCallbacks(
+                    build.getToolCallbacks()
+            ).build();
+        }
+
+        return chatClientBuilder
+                .defaultSystem(coderPrompt.getContentAsString(Charset.defaultCharset()))
                 .build();
     }
 }
--- src/main/java/com/alibaba/cloud/ai/example/graph/tool/WebSearchTool.java
+++ src/main/java/com/alibaba/cloud/ai/example/graph/tool/WebSearchTool.java
@@ -1,4 +1,23 @@
 package com.alibaba.cloud.ai.example.graph.tool;
 
+
+import com.alibaba.cloud.ai.example.graph.model.TavilySearchResponse;
+import com.alibaba.cloud.ai.example.graph.tool.search.tavily.TavilySearchApi;
+import org.springframework.ai.tool.annotation.Tool;
+import org.springframework.ai.tool.annotation.ToolParam;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class WebSearchTool {
+
+    @Autowired
+    private TavilySearchApi tavilySearchApi;
+
+    @Tool(description = "Search the web for information")
+    public String search(@ToolParam(description = "search content") String query) {
+        TavilySearchResponse search = tavilySearchApi.search(query);
+        return search.getResults().get(0).getContent();
+    }
+
 }
